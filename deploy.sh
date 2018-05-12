c36b2ae57f1c85725716fe37ff3a92dab5adc1fd
JungJinHwan <yadot1478@naver.com>
2018-05-13 00:03:05 +0900

    deploy script 수정

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,32 @@
 
 echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 
+if [ -e public/.git ]
+then
+    echo "public exists"
+    cd public/
+    git checkout .
+    git pull origin master
+    cd ..
+else
+    echo "public download..."
+    git clone https://github.com/MoonOnYou/MoonOnYou.github.io.git public
+fi
+
+if [ -e themes/minimal/.git ]
+then
+    echo "minimal exists"
+    # cd themes/minimal/
+    # git checkout .
+    # git pull origin master
+    # cd ../..
+else
+    echo "minimal download..."
+    cd themes/
+    git clone https://github.com/calintat/minimal.git minimal
+    cd ..
+fi
+
 # Build the project.
 hugo # if using a theme, replace with `hugo -t <YOURTHEME>`
 
