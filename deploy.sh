f3bb41b01928943ab86533b561161a259e82c205
JungJinHwan <yadot1478@naver.com>
2018-05-20 18:54:51 +0900

    rebuilding site 2018. 05. 20. (일) 18:54:47 KST

--- deploy.sh
+++ deploy.sh
@@ -1,29 +1,26 @@
 #!/bin/bash
 
 echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
-
-# Build the project.
-hugo
-
-# Go To Public folder
-cd public
-# Add changes to git.
-git checkout master
-git add .
-
-# Commit changes.
 msg="rebuilding site `date`"
 if [ $# -eq 1 ]
   then msg="$1"
 fi
-git commit -m "$msg"
 
-# Push source and build repos.
+# build
+hugo
+
+# public deploy
+cd public
+git checkout master
+git add .
+git commit -m "$msg"
+git pull origin master
 git push origin master
 
-# Come Back up to the Project Root
+
 cd ..
 git checkout master
 git add .
 git commit -m "$msg"
+git pull origin master
 git push origin master
