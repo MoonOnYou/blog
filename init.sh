ab89d4a7e97cde94d1c405705d258d7710d94ced
JungJinHwan <yadot1478@naver.com>
2018-05-20 18:42:19 +0900

    init.sh: 다른 환경에서 클론 이후 한번 실행하여 서브모듈 가져오는 스크립트

---
+++ init.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+git submodule init
+git submodule update
+
+if [ -e themes/minimal/.git ]
+then
+    echo "minimal exists"
+    # cd themes/minimal/
+    # git checkout .
+    # git pull origin master
+    # cd ../..
+else
+    echo "minimal download..."
+    cd themes/
+    git clone https://github.com/calintat/minimal.git minimal
+    cd ..
+fi
+git pull origin master
